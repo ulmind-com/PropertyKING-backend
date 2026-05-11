3b65733bb22afc8c82d45e9c58804ee4f57c7e0c
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2026-05-12 00:00:46 +0530

    chore: allow all CORS origins

--- app/main.py
+++ app/main.py
@@ -100,8 +100,8 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allowed_origins_list,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
